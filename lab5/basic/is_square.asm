ac99c94d35aa25941d2ea54e615ed4188674d995
swenbao <95946132+swenbao@users.noreply.github.com>
2023-11-09 17:54:24 +0800

    aa

--- lab5/basic/is_square.asm
+++ lab5/basic/is_square.asm
@@ -9,20 +9,20 @@ _is_square:
     INCF 0x010
 
     loop:
-        MOVF 0x010, W ; WREG = [0x010]
-	square:
-	    CPFSEQ 0x001 ; if WREG == [0x001] skip next line
-		GOTO not_square
-	    CLRF WREG
-	    INCF WREG ; WREG++
-	    RETURN ; return WREG
-	not_square:
-	    CPFSLT 0x001 ; if [0x001] < WREG skip next line
-		GOTO routine
-	    SETF WREG ; WREG = 0xFF
-	    RETURN ; return WREG
-	routine:    
-	    SUBWF 0x001 ; [0x001] = [0x001] - WREG
-	    INCF 0x010 ; [0x010]++
-	    INCF 0x010 ; [0x010]++
+		MOVF 0x010, W ; WREG = [0x010]
+		square:
+			CPFSEQ 0x001 ; if WREG == [0x001] skip next line
+				GOTO not_square
+			CLRF WREG
+			INCF WREG ; WREG++
+			RETURN ; return WREG
+		not_square:
+			CPFSLT 0x001 ; if [0x001] < WREG skip next line
+				GOTO routine
+			SETF WREG ; WREG = 0xFF
+			RETURN ; return WREG
+		routine:    
+			SUBWF 0x001 ; [0x001] = [0x001] - WREG
+			INCF 0x010 ; [0x010]++
+			INCF 0x010 ; [0x010]++
     GOTO loop
