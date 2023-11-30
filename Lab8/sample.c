43223af664d60be42e9e8f5ac4f8f1f210521386
swenbao <essence.lost.0b@icloud.com>
2023-11-30 10:37:58 +0800

    I'm so tired

---
+++ Lab8/sample.c
@@ -0,0 +1,47 @@
+#include <xc.h>
+#include <pic18f4520.h>
+
+#pragma config OSC = INTIO67    // Oscillator Selection bits
+#pragma config WDT = OFF        // Watchdog Timer Enable bit 
+#pragma config PWRT = OFF       // Power-up Enable bit
+#pragma config BOREN = ON       // Brown-out Reset Enable bit
+#pragma config PBADEN = OFF     // Watchdog Timer Enable bit 
+#pragma config LVP = OFF        // Low Voltage (single -supply) In-Circute Serial Pragramming Enable bit
+#pragma config CPD = OFF        // Data EEPROM　Memory Code Protection bit (Data EEPROM code protection off)
+
+void main(void)
+{
+    T2CONbits.TMR2ON = 0b1; // activate Timer2
+    T2CONbits.T2CKPS = 0b01; // prescaler = 4
+
+    // Internal Oscillator Frequency, Fosc = 125 kHz, Tosc = 8 µs
+    OSCCONbits.IRCF = 0b001;
+    
+    // PWM mode, P1A, P1C active-high; P1B, P1D active-high
+    CCP1CONbits.CCP1M = 0b1100;
+    
+    // CCP1/RC2 -> Output
+    TRISC = 0;
+    LATC = 0; // output low
+    
+    // Set up PR2, CCP to decide PWM period and Duty Cycle
+    /** 
+     * PWM period
+     * = (PR2 + 1) * 4 * Tosc * (TMR2 prescaler)
+     * = (0x9b + 1) * 4 * 8µs * 4
+     * = 0.019968s ~= 20ms
+     */
+    PR2 = 0x9b; // PR2 is a file register
+    
+    /** 
+     * Duty cycle
+     * = (CCPR1L:CCP1CON<5:4>) * Tosc * (TMR2 prescaler)
+     * = (0x0b*4 + 0b01) * 8µs * 4
+     * = 0.00144s ~= 1450µs
+     */
+    CCPR1L = 0x0b; // CCPR1L is a file register
+    CCP1CONbits.DC1B = 0b01;
+    
+    while(1);
+    return;
+}
