2d1a476e8257980b7cf0fd3900f0429d58937a9e
swenbao <essence.lost.0b@icloud.com>
2024-01-04 14:19:41 +0800

    Lab6 C version

---
+++ lab6/basic.c
@@ -0,0 +1,125 @@
+// PIC18F4520 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1H
+#pragma config OSC = INTIO67    // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 1000000
+#include <pic18f4520.h>
+
+int state = 0;
+
+void __interrupt(high_priority) H_ISR(){
+    if(INTCONbits.INT0IF == 1){ // if INT01F interrupt flag is set, representing that INT0 external interrupt occurred (must be cleared in software)
+        state++;
+        state %= 5;
+        __delay_ms(10); 
+        INTCONbits.INT0IF = 0;
+    }
+}
+
+void main(void) {
+    // set all pins to digital
+    ADCON1 = 0x0f;
+
+    // internal oscillator setup
+    //OSCCONbits.IRCF = 0b100; // set internal oscillator to 1MHz
+
+    // set up & initialize ports input output
+    // output ports
+    TRISA = 0x00;  // set RA0~PA8 to output
+    LATA = 0x00;  // set RA0~RA8 pins to low
+    // input ports
+    TRISBbits.RB0 = 1; // set RB0 to input
+
+    // set up interrupts
+    RCONbits.IPEN = 0; // disable priority levels on interrupts
+    INTCONbits.INT0IF = 0; // clear INT0F interrupt flag // 代表現在沒有 interrupt
+    INTCONbits.INT0IE = 1; // enable INT0 external interrupt // 允許 INT0 這個 interrupt 也就是 RB0 要接收 interrupt 喔！
+    INTCONbits.GIE = 1; // enable all unmasked interrupts // 告訴整個機器可以 interrupt
+
+    while(1){
+        if(state == 0){
+            LATA = 0;
+        }else if (state == 1){ // pull-up resistor
+            LATAbits.LA0 = 1;
+            LATAbits.LA1 = 0;
+            LATAbits.LA2 = 0;
+            LATAbits.LA3 = 0;
+        }else if(state == 2){
+            LATAbits.LA0 = 0;
+            LATAbits.LA1 = 1;
+            LATAbits.LA2 = 0;
+            LATAbits.LA3 = 0;
+        }else if (state == 3){
+            LATAbits.LA0 = 0;
+            LATAbits.LA1 = 0;
+            LATAbits.LA2 = 1;
+            LATAbits.LA3 = 0;
+        }else if (state == 4){
+            LATAbits.LA0 = 0;
+            LATAbits.LA1 = 0;
+            LATAbits.LA2 = 0;
+            LATAbits.LA3 = 1;
+        }
+    };
+    return;
+}
+
